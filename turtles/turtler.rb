e53e4b35cc256a5e59b4363fc4e9427f8abf20ac
R. Tyler Croy <tyler@monkeypox.org>
2014-09-28 17:43:39 -0700

    Use colorize within the turtle demo for prettier/more legible output

--- turtles/turtler.rb
+++ turtles/turtler.rb
@@ -4,6 +4,9 @@
 # <https://github.com/jruby/jruby/wiki/DirectJRubyEmbedding>
 #
 
+# For pretties
+require 'colorize'
+
 java_import org.jruby.Ruby
 java_import org.jruby.RubyRuntimeAdapter
 java_import org.jruby.javasupport.JavaEmbedUtils
@@ -13,19 +16,24 @@ runtime = JavaEmbedUtils.initialize(java.util.ArrayList.new)
 
 # Create our evaluator
 evaler = JavaEmbedUtils.new_runtime_adapter
+# We want our pretties inside the embedded runtime too!
+evaler.eval(runtime, <<-END
+require 'colorize'
+puts
+            END
+)
 
 # Appending 'nil' to the end of these statements to prevent Pry from attempting
 # to inspect the returned object
-evaler.eval(runtime, 'puts; puts "Hello JavaOne, I\'m embedded!"; puts'); nil
+evaler.eval(runtime, 'puts "Hello JavaOne, I\'m embedded!".colorize(:green); puts'); nil
 
 
 require './turtles/first'
-puts "Our parent's scope is version: #{JavaOne::VERSION}"
+puts "Our original JRuby has version: #{JavaOne::VERSION}".colorize(:magenta)
+puts
 
 # Now let's load a the same module with a different version inside the runtime
 # environment
 evaler.eval(runtime, "require './turtles/second'"); nil
-evaler.eval(runtime, "puts \"Our child scope is: \#{JavaOne::VERSION}\""); nil
-
-
+evaler.eval(runtime, "puts \"Our embedded JRuby has version: \#{JavaOne::VERSION}\".colorize(:green)"); nil
 
